709635161dbcd02dc5df0a02f24311bdb6a51b2f
Yang <zhy0216@gmail.com>
2016-03-21 16:03:47 -0400

    implement basic function

--- scheme.js
+++ scheme.js
@@ -6,8 +6,6 @@ var _ = require("underscore");
 function interpreter(expr){
     var parsedData = parse(expr);
     var env = {
-        '+': (x,y) => x + y,
-
     }
 
     return _inter(parsedData, env);
@@ -17,20 +15,33 @@ function lookup(symbol, env){
     if(_.has(env, symbol)){
         return env[symbol]
     }
-    throw new Error('no ' + symbol + "in env");
+    // console.log(env);
+    throw new Error('no ' + symbol + " in env");
 }
 
 function _inter(expr, env){
+    // console.log(expr);
+    // console.log(env);
     if(_.isArray(expr)){
         if(expr.length === 1){
-            return _inter(expr[0])
+            return _inter(expr[0], env)
         }
 
         if(_.isString(expr[0])){
             if(expr[0] == '+'){
-                return _inter(expr[1]) + _inter(expr[2]);
+                return _inter(expr[1], env) + _inter(expr[2], env);
+            }
+
+            if(expr[0] == 'lambda'){
+                return function(x){
+                    env[expr[1][0]] = x;
+                    return _inter(expr[2], env);
+                }
             }
         }
+
+        return _inter(expr[0], env)(_inter(expr[1], env))
+
     }
     if(_.isNumber(expr) || _.isBoolean(expr)){
         return expr;
@@ -46,7 +57,8 @@ function _inter(expr, env){
 
 console.log(interpreter("(+ 1 2)")) // ==> 3
 console.log(interpreter("(+ 1 (+ 1 2))")) // ==> 4
-
+console.log(interpreter("(lambda (x) x)")) // ==> [Function]
+console.log(interpreter("((lambda (x) (+ x 2)) 1)")) // ==> 3
 
 
 
